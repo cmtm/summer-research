bfe5fab3a5f21488e29d98c90b0ffbe8982014f1
Chris Morin <chris.morin2@gmail.com>
2013-08-12 18:44:37 -0400

    aam test compiles but doesn't work since it doesn't use mmap. Am  going to add it.

--- src/arm/aam_test_compat.cpp
+++ src/arm/aam_test_compat.cpp
@@ -9,6 +9,12 @@
 #define aam_port 0x70000000
 #define erasure_val 0xFFFFFFFF
 
+#define FATAL do { fprintf(stderr, "Error at line %d, file %s (%d) [%s]\n", \
+  __LINE__, __FILE__, errno, strerror(errno)); exit(1); } while(0)
+
+#define MAP_SIZE 4096UL
+#define MAP_MASK (MAP_SIZE - 1)
+
 enum Cmd {READ = 0, STORE = 1};
 
 template <class InputIterator>
@@ -43,9 +49,8 @@ readMessage(InputIterator msg) {
 	outl_s(aam_port + 1, msg, messageLength);
 	outl(READ, aam_port);
 	int erasures = std::count (msg, msg + messageLength, erasure_val);
-	auto respsPerCluster(inl_s(aam_port, erasures));
-
-	return respsPerCluster;
+	
+	return inl_s(aam_port, erasures);
 }
 
 std::vector<ulong> 
@@ -57,8 +62,7 @@ getRespsForCluster(const std::vector<ulong>& respPerCluster,
 	ulong offset;
 
 	offset = respPerCluster.size();
-	auto start = respPerCluster.begin();
-	offset += std::accumulate(start, start + erasurePos, 0);
+	offset += std::accumulate(respPerCluster.begin(), respPerCluster.begin() + erasurePos, 0);
 
 
 	return inl_s(aam_port + offset, respPerCluster[erasurePos]);
@@ -75,14 +79,15 @@ int main(void) {
 	std::cout << "Response from ioperm: " << r << std::endl;
 
 	//let's store a message
-	std::vector<ulong> myMessage({4,7,2,4});
+	std::vector<ulong> myMessage;
+	myMessage.push_back(4); myMessage.push_back(7); myMessage.push_back(2); myMessage.push_back(4);
 	storeMessage(myMessage.begin());
 	std::cout << "Message [4,7,2,4] written." << std::endl;
 
 	//let's see if read works properly
 	eraseMessage(myMessage, 0);
 	eraseMessage(myMessage, 3);
-	auto rslt = readMessage(myMessage.begin());
+	std::vector<ulong> rslt = readMessage(myMessage.begin());
 	std::cout << "[X,7,2,X] read" << std::endl;
 	std::cout << "Contends of result from read: size= "
 	          <<rslt.size()<<" vals= ("<<rslt[0]<<", "<<rslt[1]<<")"<<std::endl;
